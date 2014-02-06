ce93ca0c6532c4b7e2587d9a7f5c7ef021963379
Ryan Barnett <ryan@barnett.io>
2014-02-06 01:13:55 -0500

    Refactored for Efficiency

--- Numbers/FactorialFinder.py
+++ Numbers/FactorialFinder.py
@@ -1,14 +1,4 @@
 __author__ = 'barnett'
-'''
-def factorial(n):
-    i = n
-    if n < 1:
-        return 1
-    while n > 1:
-        n -= 1
-        i = n * i
-    return i
-'''
 #refactored factorial(n) for efficiency
 def factorial(n):
     result = 1
